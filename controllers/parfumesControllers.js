596c4c527038a299cd73d66c267e758c773c1a36
alessiaimprota <alessiaimprota126@gmail.com>
2026-03-09 12:59:53 +0100

    feat show route

--- controllers/parfumesControllers.js
+++ controllers/parfumesControllers.js
@@ -10,5 +10,40 @@ function index(req, res) {
     res.json(results);
   });
 }
+
+function show(req, res) {
+  const { public_slug } = req.params;
+
+  const parfumesSql = "SELECT * FROM products WHERE public_slug = ?";
+
+  connection.query(parfumesSql, [public_slug], (err, parfumeResult) => {
+    if (err) return res.status(500).json({ error: "Database query failed" });
+    if (parfumeResult.length === 0)
+      return res.status(404).json({ error: "Profumo non trovato" });
+
+    const parfume = parfumeResult[0];
+    const parfumeId = parfume.id;
+
+    const imagesSql = "SELECT * FROM  product_images WHERE product_id = ? ";
+    connection.query(imagesSql, [parfumeId], (err, imageResult) => {
+      if (err) return res.status(500).json({ error: "Database query failed" });
+      parfume.images = imageResult;
+
+      const notesSql = `
+        SELECT notes.* 
+        FROM notes
+        JOIN note_product ON notes.id = note_product.note_id
+        WHERE note_product.product_id = ?
+      `;
+      connection.query(notesSql, [parfumeId], (err, notesResult) => {
+        if (err)
+          return res.status(500).json({ error: "Database query failed" });
+        parfume.notes = notesResult;
+
+        res.json(parfume);
+      });
+    });
+  });
+}
 //esporto
-module.exports = { index };
+module.exports = { index, show };
