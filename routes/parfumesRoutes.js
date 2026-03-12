4ed69c578367304fb65119074e6708ab652acdbf
CesareFortunato <cesare.fortunato89@gmail.com>
2026-03-12 11:00:08 +0100

    fix: route name

--- routes/parfumesRoutes.js
+++ routes/parfumesRoutes.js
@@ -9,6 +9,6 @@ router.get("/:public_slug", parfumesControllers.show);
 //e metto le note sempre nella rotta delle profuziomazioni sperando sia coerente comunque
 router.get("/note/:noteId", parfumesControllers.getNote);
 //rotta per correlati
-router.get("/parfumes/:public_slug/related", parfumesControllers.related);
+router.get("/:public_slug/related", parfumesControllers.related);
 
 module.exports = router;
