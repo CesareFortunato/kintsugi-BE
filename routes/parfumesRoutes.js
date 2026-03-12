ce0ca9210016ccc9f42a16c6ca32722606372647
alessiaimprota <alessiaimprota126@gmail.com>
2026-03-12 12:01:18 +0100

    feat/search

--- routes/parfumesRoutes.js
+++ routes/parfumesRoutes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const parfumesControllers = require("../controllers/parfumesControllers");
 
 router.get("/", parfumesControllers.index);
+
+router.get("/search", parfumesControllers.search);
 //utilizzo il public slug invece dell'id perché messo nel db
 router.get("/:public_slug", parfumesControllers.show);
 //e metto le note sempre nella rotta delle profuziomazioni sperando sia coerente comunque
